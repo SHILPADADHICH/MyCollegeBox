ae0cb7ad3cca4c5787cfc6a245d4543c0d8632de
SHILPADADHICH <shilpadadhich52@gmail.com>
2025-06-27 23:22:35 +0530

    refactor: clean up layout files and ensure proper splash screen handling

--- app/(tabs)/_layout.tsx
+++
@@ -1,5 +0,0 @@
-<<<<<<< HEAD
-export { default } from "./messages";
-=======
-export { default } from "../messages";
->>>>>>> ecc88cfa7bb885ef75ead6cd1bc7a40d9c2bd0f0
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,17 +1,14 @@
-import { Stack } from 'expo-router';
-import { useFonts } from 'expo-font';
-import * as SplashScreen from 'expo-splash-screen';
+import { Stack } from "expo-router";
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
 import {
   Poppins_400Regular,
   Poppins_600SemiBold,
   Poppins_700Bold,
-} from '@expo-google-fonts/poppins';
-import { useCallback } from 'react';
-import { View } from 'react-native';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
-
-
-SplashScreen.preventAutoHideAsync();
+} from "@expo-google-fonts/poppins";
+import { useCallback, useEffect } from "react";
+import { View } from "react-native";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
 export default function RootLayout() {
   const [fontsLoaded] = useFonts({
@@ -19,6 +16,10 @@ export default function RootLayout() {
     Poppins_600SemiBold,
     Poppins_700Bold,
   });
+
+  useEffect(() => {
+    SplashScreen.preventAutoHideAsync();
+  }, []);
 
   const onLayoutRootView = useCallback(async () => {
     if (fontsLoaded) {
@@ -30,16 +31,9 @@ export default function RootLayout() {
 
   return (
     <SafeAreaProvider>
-<<<<<<< HEAD
-      
-=======
-     
-   
->>>>>>> ecc88cfa7bb885ef75ead6cd1bc7a40d9c2bd0f0
-    <View style={{ flex: 1 }} onLayout={onLayoutRootView}>
-    
-      <Stack screenOptions={{ headerShown: false }} />
-    </View>
-     </SafeAreaProvider>
+      <View style={{ flex: 1 }} onLayout={onLayoutRootView}>
+        <Stack screenOptions={{ headerShown: false }} />
+      </View>
+    </SafeAreaProvider>
   );
 }
